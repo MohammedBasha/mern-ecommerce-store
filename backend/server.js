ae37216a32afb01a67d67fbffa01fe6ff548ab20
MohammedBasha <me@mohammedbasha.com>
2026-05-14 20:04:40 +0300

    initializing the project for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // Import database connection
 import { connectDB } from "./lib/db.js";
@@ -17,6 +18,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json({ limit: "10mb" }));
@@ -28,6 +31,14 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponsRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
